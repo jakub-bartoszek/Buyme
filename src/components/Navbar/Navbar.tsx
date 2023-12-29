7c8698a80616315a62f2116585cbf82612059df6
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-12-29 16:29:41 +0100

    implemented tsx logic

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,56 +1,60 @@
 import "./styles.scss";
 import { HeartIcon } from "@heroicons/react/24/outline";
 import { ShoppingCartIcon } from "@heroicons/react/24/outline";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { Bars3Icon } from "@heroicons/react/24/solid";
 
-export const Navbar = () => (
- <nav className="navbar">
-  <div className="navbar__content">
-   <div className="navbar__logoWrapper">
-    <Bars3Icon className="navbar__bars" />
-    <a className="navbar__logo">
-     <h1>BuyMe!</h1>
-    </a>
+interface NavbarProps {}
+
+const Navbar: React.FC<NavbarProps> = () => {
+ return (
+  <nav className="navbar">
+   <div className="navbar__content">
+    <div className="navbar__logoWrapper">
+     <Bars3Icon className="navbar__bars" />
+     <a
+      href="/"
+      className="navbar__logo"
+     >
+      <h1>BuyMe!</h1>
+     </a>
+    </div>
+    <input
+     className="navbar__menu--searchInput"
+     type="text"
+     placeholder="Search"
+    />
+    <div className="navbar__menu--links">
+     {["Home", "Shop", "About", "Contact"].map((link, index) => (
+      <a
+       key={index}
+       href="/"
+       className="navbar__menu--link"
+      >
+       {link}
+      </a>
+     ))}
+    </div>
+    <div className="navbar__menu--buttons">
+     <div className="navbar__menu--button">
+      <MagnifyingGlassIcon />
+     </div>
+     <a
+      href="/"
+      className="navbar__menu--button"
+     >
+      <HeartIcon />
+     </a>
+     <a
+      href="/"
+      className="navbar__menu--button"
+     >
+      <ShoppingCartIcon />
+     </a>
+    </div>
    </div>
-   <input></input>
-   <div className="navbar__menu">
-    <a
-     href=""
-     className="navbar__menu--link"
-    >
-     Home
-    </a>
-    <a
-     href=""
-     className="navbar__menu--link"
-    >
-     Shop
-    </a>
-    <a
-     href=""
-     className="navbar__menu--link"
-    >
-     About
-    </a>
-    <a
-     href=""
-     className="navbar__menu--link"
-    >
-     Contact
-    </a>
-    <a
-     href=""
-     className="navbar__menu--link"
-    >
-     <HeartIcon />
-    </a>
-    <a
-     href=""
-     className="navbar__menu--link"
-    >
-     <ShoppingCartIcon />
-    </a>
-   </div>
-  </div>
- </nav>
-);
+  </nav>
+ );
+};
+
+export default Navbar;
