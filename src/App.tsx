a4846089700a36df25d47b9eb63ef13a0c3d641f
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-12-29 16:29:24 +0100

    added imitation of banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import { Navbar } from "./components/Navbar/Navbar";
+import Navbar from "./components/Navbar/Navbar";
+import "./styles/styles.scss";
 
 function App() {
  return (
   <div>
    <Navbar />
-   <header></header>
-   <main></main>
-   <footer></footer>
+   <div className="wrapper">
+    <header>
+     <div className="banner"></div>
+    </header>
+    <main></main>
+    <footer></footer>
+   </div>
   </div>
  );
 }
