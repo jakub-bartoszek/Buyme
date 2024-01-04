d1f1219096ad3e11b847602a27fd0057f5c662ec
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-01-04 15:33:35 +0100

    created structure of site

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import "./styles/styles.scss";
 import Home from "./pages/Home/Home";
+import Shop from "./pages/Shop/Shop";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Cart from "./pages/Cart/Cart";
+import Favourites from "./pages/Favourites/Favourites";
+import Search from "./pages/Search.tsx/Search";
 
 function App() {
  return (
@@ -13,6 +19,30 @@ function App() {
       <Route
        path="/home"
        element={<Home />}
+      />
+      <Route
+       path="/shop"
+       element={<Shop />}
+      />
+      <Route
+       path="/about"
+       element={<About />}
+      />
+      <Route
+       path="/contact"
+       element={<Contact />}
+      />
+      <Route
+       path="/search"
+       element={<Search />}
+      />
+      <Route
+       path="/favorites"
+       element={<Favourites />}
+      />
+      <Route
+       path="/cart"
+       element={<Cart />}
       />
       <Route
        path="/"
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+ return (
+  <div>
+   <header>banner</header>
+   <main>Home</main>
+  </div>
+ );
+}
