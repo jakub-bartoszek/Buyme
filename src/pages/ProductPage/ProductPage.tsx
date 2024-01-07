6f555c78d36a11c7de7faf26eefdc779f5a89312
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-01-07 15:48:19 +0100

    conditional styles for buttons

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -7,6 +7,8 @@ export default function ProductPage() {
  const [product, setProduct] = useState<Product | null>(null);
  const { id } = useParams();
  const [activeImageIndex, setActiveImageIndex] = useState<number | null>(null);
+ const [chosenSize, setChosenSize] = useState<string | null>(null);
+ const [amount, setAmount] = useState<number>(0);
 
  const fetchProducts = async () => {
   try {
@@ -43,54 +45,74 @@ export default function ProductPage() {
  };
 
  return (
-  <main className="wrapper">
-   <div className="product">
-    <div className="product-gallery">
-     <div className="product-gallery__images-small">
-      {product?.images &&
-       product.images.map((image, index) => (
-        <div
-         className={`product-gallery__image-small ${
-          index === activeImageIndex ? "active" : ""
-         }`}
-         key={index}
-        >
-         <img
-          src={image}
-          onClick={() => handleImageClick(index)}
-          alt={`Product Image ${index}`}
-         />
-        </div>
-       ))}
-     </div>
-     {activeImageIndex !== null && product?.images && (
-      <div className="product-gallery__image-main">
-       <img
-        src={product.images[activeImageIndex]}
-        alt="Main Product Image"
-       />
-      </div>
-     )}
-    </div>
-    <div className="product-info">
-     <h2>{product?.name}</h2>
-     <p>{product?.price} $</p>
-     <div className="product-info__sizes">
-      {product?.available_sizes.map((size) => (
-       <button className="product-info__size">{size}</button>
+  <main className="product">
+   <div className="product-gallery">
+    <div className="product-gallery__images-small">
+     {product?.images &&
+      product.images.map((image, index) => (
+       <div
+        className={`product-gallery__image-small ${
+         index === activeImageIndex ? "active" : ""
+        }`}
+        key={index}
+       >
+        <img
+         src={image}
+         onClick={() => handleImageClick(index)}
+         alt={`Product Image ${index}`}
+        />
+       </div>
       ))}
-     </div>
-     <div className="product-info__amount">
-      <button className="product-info__amount--button">-</button>
-      <div className="product-info__amount--number">0</div>
-      <button className="product-info__amount--button">+</button>
-     </div>
-     <div>
-      <button className="product-info__buy-button">ADD TO CART</button>
-     </div>
     </div>
-    <div className="product-description">{product?.description}</div>
+    {activeImageIndex !== null && product?.images && (
+     <div className="product-gallery__image-main">
+      <img
+       src={product.images[activeImageIndex]}
+       alt="Main Product Image"
+      />
+     </div>
+    )}
    </div>
+   <div className="product-info">
+    <h2>{product?.name}</h2>
+    <p>{product?.price} $</p>
+    <div className="product-info__sizes">
+     {product?.available_sizes.map((size) => (
+      <button
+       className={`product-info__size ${size == chosenSize && "chosen"}`}
+       onClick={() => setChosenSize(size)}
+      >
+       {size}
+      </button>
+     ))}
+    </div>
+    <div className="product-info__amount">
+     <button
+      disabled={amount === 0}
+      className="product-info__amount--button"
+      onClick={() => amount > 0 && setAmount((amount) => amount - 1)}
+     >
+      -
+     </button>
+     <div className="product-info__amount--number">{amount}</div>
+     <button
+      disabled={amount === 10}
+      className="product-info__amount--button"
+      onClick={() => amount < 10 && setAmount((amount) => amount + 1)}
+     >
+      +
+     </button>
+    </div>
+    <div>
+     <button
+      disabled={chosenSize == null || amount == 0}
+      className="product-info__buy-button"
+     >
+      ADD TO CART
+     </button>
+    </div>
+   </div>
+   <div className="product-description">{product?.description}</div>
   </main>
  );
 }
