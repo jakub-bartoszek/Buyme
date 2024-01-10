26ecbd7d0ad0bda2b1826e79d5d819f9365a7844
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-01-10 16:14:57 +0100

    displaying product info in cart

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -12,6 +12,12 @@ export default function Cart() {
  const cart: CartItem[] = storedCart ? JSON.parse(storedCart) : [];
  const products: Product[] = useSelector(selectProducts);
 
+ console.log(
+  cart[2].sizes
+   .map((size) => size.amount)
+   .reduce((partialSum, a) => partialSum + a, 0)
+ );
+
  return (
   <div className="cart">
    <div className="products">
@@ -26,18 +32,41 @@ export default function Cart() {
         <div className="products__tile--image">
          {product.images && <img src={product.images[0]} />}
         </div>
-        <div>
-         <h3 className="products__tile--name">{product.name}</h3>
-         {item.sizes.map((size) => (
-          <p className="products__tile--sizes">
-           {size.name}: {size.amount}
-          </p>
-         ))}
-        </div>
+        <table className="products__tile--table">
+         <thead>
+          <tr>
+           <th>{product.name}</th>
+           <th>Each</th>
+           <th>Total</th>
+          </tr>
+         </thead>
+         <tbody>
+          <tr>
+           <td>
+            {item.sizes.map((size) => (
+             <p
+              key={size.name}
+              className="products__tile--sizes"
+             >
+              {size.name}: {size.amount}
+             </p>
+            ))}
+           </td>
+           <td>{product.price}</td>
+           <td>
+            {product.price *
+             item.sizes
+              .map((size) => size.amount)
+              .reduce((partialSum, a) => partialSum + a, 0)}
+           </td>
+          </tr>
+         </tbody>
+        </table>
        </div>
       ))
     )}
    </div>
+   <div></div>
   </div>
  );
 }
