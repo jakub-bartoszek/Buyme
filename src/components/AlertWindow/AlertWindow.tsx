4e224f9ded95cac057800eee9071865b4facf883
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-01-16 17:06:18 +0100

    created alert window component

---
+++ src/components/AlertWindow/AlertWindow.tsx
@@ -0,0 +1,35 @@
+import "./styles.scss";
+
+const AlertWindow = ({
+ title,
+ confirmFunction,
+ cancelFunction
+}: {
+ title: string;
+ confirmFunction: () => void;
+ cancelFunction: () => void;
+}) => {
+ return (
+  <div className="alert-window-shadow">
+   <div className="alert-window">
+    <h2 className="alert-window__title">{title}</h2>
+    <div className="alert-window__buttons">
+     <button
+      className="alert-window__button"
+      onClick={() => cancelFunction()}
+     >
+      Cancel
+     </button>
+     <button
+      className="alert-window__button"
+      onClick={() => confirmFunction()}
+     >
+      Yes
+     </button>
+    </div>
+   </div>
+  </div>
+ );
+};
+
+export default AlertWindow;
