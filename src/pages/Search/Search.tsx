cc04b19118251081eae3eda313093862a3849feb
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-01-25 17:41:17 +0100

    added multirange slider for price range

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,10 +1,12 @@
 import "./styles.scss";
+import "./multirangeslider.css";
 import { useSearchParams } from "react-router-dom";
 import { Product } from "../../App";
 import { useSelector } from "react-redux";
 import { selectProducts } from "../../redux/productsSlice";
 import Tile from "../../components/Tile/Tile";
 import { useState, useEffect } from "react";
+import MultiRangeSlider, { ChangeResult } from "multi-range-slider-react";
 
 const Search: React.FC = () => {
  const [searchParams, setSearchParams] = useSearchParams();
@@ -39,6 +41,11 @@ const Search: React.FC = () => {
   setSearchParams(newSearchParams);
  };
 
+ const handleInput = (e: ChangeResult) => {
+  setMinPrice(e.minValue);
+  setMaxPrice(e.maxValue);
+ };
+
  const filteredProducts = query
   ? products.filter(
      (product) =>
@@ -49,44 +56,65 @@ const Search: React.FC = () => {
   : products;
 
  return (
-  <div className="search">
-   <main className="main">
-    <h2 className="query">Search results for "{query}"</h2>
+  <main className="search">
+   <p className="query">{`Search results for "${query}" (${filteredProducts.length})`}</p>
+   <div>
     <form
      className="filters"
      onSubmit={handleSubmit}
     >
-     <div>
-      Price from &nbsp;
-      <input
-       className="filters__field"
-       type="number"
-       value={minPrice || ""}
-       onChange={(e) => setMinPrice(parseInt(e.target.value, 10))}
+     <div className="filters__price">
+      Price
+      <MultiRangeSlider
+       min={0}
+       max={500}
+       step={5}
+       minValue={minPrice ? minPrice : 0}
+       maxValue={maxPrice ? maxPrice : 500}
+       ruler={false}
+       label={false}
+       onInput={(e) => handleInput(e)}
+       baseClassName="filters__price--range-slider"
       />
-      &nbsp;to&nbsp;
-      <input
-       className="filters__field"
-       type="number"
-       value={maxPrice || ""}
-       onChange={(e) => setMaxPrice(parseInt(e.target.value, 10))}
-      />
+     </div>
+     <div className="filters__price--values-container">
+      <div className="filters__price--field">
+       <span>$</span>
+       <input
+        min={0}
+        max={maxPrice ? maxPrice : 500}
+        type="number"
+        value={minPrice !== null ? minPrice : ""}
+        onChange={(e) => setMinPrice(parseInt(e.target.value, 10))}
+       />
+      </div>
+      <span>to</span>
+      <div className="filters__price--field">
+       <span>$</span>
+       <input
+        min={minPrice ? minPrice : 0}
+        max={500}
+        type="number"
+        value={maxPrice !== null ? maxPrice : ""}
+        onChange={(e) => setMaxPrice(parseInt(e.target.value, 10))}
+       />
+      </div>
      </div>
      <button className="filters__submit-button">Apply</button>
     </form>
-    <section
-     className="section"
-     id="results"
-    >
-     {filteredProducts.map((product) => (
-      <Tile
-       key={product.id}
-       product={product}
-      />
-     ))}
-    </section>
-   </main>
-  </div>
+   </div>
+   <section
+    className="section"
+    id="results"
+   >
+    {filteredProducts.map((product) => (
+     <Tile
+      key={product.id}
+      product={product}
+     />
+    ))}
+   </section>
+  </main>
  );
 };
 
