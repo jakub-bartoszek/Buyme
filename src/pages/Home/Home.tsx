20785d63a7b2e1d2c722e5fa3905b4b6c74f65c5
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-01-26 16:45:18 +0100

    lazy loading of products in Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./styles.scss";
 import Tile from "../../components/Tile/Tile";
 import { useSelector } from "react-redux";
-import { selectProducts } from "../../redux/productsSlice";
+import { selectProducts } from "../../utils/redux/productsSlice";
 import { Product } from "../../App";
 
 const Home: React.FC = () => {
@@ -16,50 +16,64 @@ const Home: React.FC = () => {
      alt="Banner"
     />
    </header>
-   <main className="main">
-    <h2 className="section-title">Newest</h2>
-    <section
-     className="section"
-     id="newest"
-    >
-     {products
-      .filter((product) => product?.date_added.includes("2024"))
-      .map((product) => (
-       <Tile
-        key={product.id}
-        product={product}
-       />
+   {products.length > 0 ? (
+    <main className="main">
+     <h2 className="section-title">Newest</h2>
+     <section
+      className="section"
+      id="newest"
+     >
+      {products
+       .filter((product) => product?.date_added.includes("2024"))
+       .map((product) => (
+        <Tile
+         key={product.id}
+         product={product}
+        />
+       ))}
+     </section>
+     <h2 className="section-title">Most popular</h2>
+     <section
+      className="section"
+      id="most_popular"
+     >
+      {products
+       .filter((product) => product?.popularity > 80)
+       .map((product) => (
+        <Tile
+         key={product.id}
+         product={product}
+        />
+       ))}
+     </section>
+     <h2 className="section-title">Winter collection</h2>
+     <section
+      className="section"
+      id="winter_collection"
+     >
+      {products
+       .filter((product) => product?.categories?.includes("winter"))
+       .map((product) => (
+        <Tile
+         key={product.id}
+         product={product}
+        />
+       ))}
+     </section>
+    </main>
+   ) : (
+    <main className="main">
+     <h2 className="section-title">Loading...</h2>
+     <section
+      className="section"
+      id="newest"
+     >
+      {[...Array(21).keys()].map(() => (
+       <Tile loading={true} />
       ))}
-    </section>
-    <h2 className="section-title">Most popular</h2>
-    <section
-     className="section"
-     id="most_popular"
-    >
-     {products
-      .filter((product) => product?.popularity > 80)
-      .map((product) => (
-       <Tile
-        key={product.id}
-        product={product}
-       />
-      ))}
-    </section>
-    <h2 className="section-title">Winter collection</h2>
-    <section
-     className="section"
-     id="winter_collection"
-    >
-     {products
-      .filter((product) => product?.categories?.includes("winter"))
-      .map((product) => (
-       <Tile
-        key={product.id}
-        product={product}
-       />
-      ))}
-    </section>
-   </main>
+     </section>
+    </main>
+   )}
   </div>
  );
 };
