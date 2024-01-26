7e59f2eaacdcb28fab6934b879c691de93b7fed8
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-01-26 16:45:26 +0100

    changed import of redux

--- src/pages/Favourites/Favourites.tsx
+++ src/pages/Favourites/Favourites.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addToFavourites, selectFavourites } from "../../redux/favouritesSlice";
+import { addToFavourites, selectFavourites } from "../../utils/redux/favouritesSlice";
 import { Product } from "../../App";
-import { selectProducts } from "../../redux/productsSlice";
+import { selectProducts } from "../../utils/redux/productsSlice";
 import "./styles.scss";
 import { HeartIcon } from "@heroicons/react/24/solid";
 import { Link } from "react-router-dom";
