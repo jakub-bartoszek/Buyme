96a11d2fcff9804cedd5a8a1e2341065240b7d57
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-01-28 22:28:13 +0100

    added filtering by categories

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,41 +1,37 @@
+import { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
+import MultiRangeSlider, { ChangeResult } from "multi-range-slider-react";
+
+import Tile from "../../components/Tile/Tile";
+import { selectProducts } from "../../utils/redux/productsSlice";
+import { Product } from "../../App";
 import "./styles.scss";
 import "./multirangeslider.scss";
-import { useSearchParams } from "react-router-dom";
-import { Product } from "../../App";
-import { useSelector } from "react-redux";
-import { selectProducts } from "../../utils/redux/productsSlice";
-import Tile from "../../components/Tile/Tile";
-import { useState, useEffect } from "react";
-import MultiRangeSlider, { ChangeResult } from "multi-range-slider-react";
-import { ChevronDownIcon } from "@heroicons/react/24/outline";
 
 const Search: React.FC = () => {
- type FilterState = {
-  visible: boolean;
-  rotated: boolean;
- };
-
- type FilterStates = {
-  price: FilterState;
-  categories: FilterState;
- };
-
  const [searchParams, setSearchParams] = useSearchParams();
  const query = searchParams.get("q");
  const minPriceParam = parseInt(searchParams.get("minPrice") || "", 10);
  const maxPriceParam = parseInt(searchParams.get("maxPrice") || "", 10);
+ const categoriesParam = (searchParams.get("categories") || "").split(",");
  const products: Product[] = useSelector(selectProducts);
 
  const [minPrice, setMinPrice] = useState<number>(0);
  const [maxPrice, setMaxPrice] = useState<number>(500);
  const [minPriceSlider, setMinPriceSlider] = useState<number>(0);
  const [maxPriceSlider, setMaxPriceSlider] = useState<number>(500);
+ const [categories, setCategories] = useState<string[]>([]);
 
- const [filterStates, setFilterStates] = useState<FilterStates>({
-  price: { visible: false, rotated: false },
-  categories: { visible: false, rotated: false }
+ const [filterStates, setFilterStates] = useState({
+  price: { visible: true },
+  categories: { visible: false }
  });
 
+ const allCategories = Array.from(new Set(products.flatMap((product) => product.categories)));
+
+ // Set initial price values based on search parameters
  useEffect(() => {
   if (!searchParams.has("minPrice") && !searchParams.has("maxPrice")) {
    setMinPrice(0);
@@ -50,7 +46,17 @@ const Search: React.FC = () => {
   }
  }, [searchParams, minPriceParam, maxPriceParam]);
 
- const handleFilterToggle = (filterType: keyof FilterStates) => {
+ // Set initial category values based on search parameters
+ useEffect(() => {
+  if (searchParams.has("categories")) {
+   setCategories((searchParams.get("categories") || "").split(","));
+  } else {
+   setCategories([]);
+  }
+ }, [searchParams]);
+
+ // Handle filter toggle
+ const handleFilterToggle = (filterType: keyof typeof filterStates) => {
   setFilterStates((prevStates) => ({
    ...prevStates,
    [filterType]: {
@@ -60,6 +66,7 @@ const Search: React.FC = () => {
   }));
  };
 
+ // Handle form submission
  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
   e.preventDefault();
 
@@ -72,10 +79,17 @@ const Search: React.FC = () => {
    if (maxPrice) newSearchParams.set("maxPrice", maxPrice.toString());
    else newSearchParams.delete("maxPrice");
 
+   if (categories.length > 0) {
+    newSearchParams.set("categories", categories.join(","));
+   } else {
+    newSearchParams.delete("categories");
+   }
+
    setSearchParams(newSearchParams);
   }
  };
 
+ // Handle slider input
  const handleSliderInput = (e: ChangeResult) => {
   setMinPrice(e.minValue);
   setMaxPrice(e.maxValue);
@@ -85,6 +99,7 @@ const Search: React.FC = () => {
   }
  };
 
+ // Handle min price change
  const handleMinPriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
   const value = parseInt(e.target.value, 10);
   if (!isNaN(value)) {
@@ -96,15 +111,13 @@ const Search: React.FC = () => {
    } else {
     setMinPrice(value <= maxPrice - 20 ? Math.max(0, value) : maxPrice - 20);
     setMinPriceSlider(
-     Math.min(
-      value <= maxPrice - 20 ? Math.max(0, value) : maxPrice - 20,
-      maxPrice
-     )
+     Math.min(value <= maxPrice - 20 ? Math.max(0, value) : maxPrice - 20, maxPrice)
     );
    }
   }
  };
 
+ // Handle max price change
  const handleMaxPriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
   const value = parseInt(e.target.value, 10);
   if (!isNaN(value)) {
@@ -113,14 +126,28 @@ const Search: React.FC = () => {
   }
  };
 
+ // Handle category change
+ const handleCategoryChange = (category: string) => {
+  setCategories((prevCategories) => {
+   if (prevCategories.includes(category)) {
+    return prevCategories.filter((c) => c !== category);
+   } else {
+    return [...prevCategories, category];
+   }
+  });
+ };
+
+ // Filter products based on search query and applied filters
  const filteredProducts = query
-  ? products.filter(
+ ? products.filter(
      (product) =>
-      product?.name.toLowerCase().includes(query.toLowerCase()) &&
-      (minPriceParam ? product?.price > minPriceParam : true) &&
-      (maxPriceParam ? product?.price < maxPriceParam : true)
-    )
-  : products;
+       product?.name.toLowerCase().includes(query.toLowerCase()) &&
+       (minPriceParam ? product?.price > minPriceParam : true) &&
+       (maxPriceParam ? product?.price < maxPriceParam : true) &&
+       ((categoriesParam.length === 0 || categoriesParam[0] === "") ||
+         product?.categories.some((cat) => categoriesParam.includes(cat)))
+   )
+ : products;
 
  return (
   <main className="search">
@@ -130,25 +157,17 @@ const Search: React.FC = () => {
      className="filters"
      onSubmit={handleSubmit}
     >
-     <div
-      className={`filters__filter ${filterStates.price.visible ? "shown" : ""}`}
-     >
+     <div className={`filters__filter ${filterStates.price.visible ? "shown" : ""}`}>
       <div className="filters__filter--header">
        <span>Price</span>
        <div
-        className={`filters__filter--show-icon ${
-         filterStates.price.visible ? "rotated" : ""
-        }`}
+        className={`filters__filter--show-icon ${filterStates.price.visible ? "rotated" : ""}`}
         onClick={() => handleFilterToggle("price")}
        >
         <ChevronDownIcon />
        </div>
       </div>
-      <div
-       className={`filters__filter--content ${
-        filterStates.price.visible ? "shown" : ""
-       }`}
-      >
+      <div className={`filters__filter--content ${filterStates.price.visible ? "shown" : ""}`}>
        <div className="filters__filter--range-slider-container">
         <MultiRangeSlider
          min={0}
@@ -163,11 +182,7 @@ const Search: React.FC = () => {
         />
        </div>
        <div className="filters__filter--values-container">
-        <div
-         className={`filters__filter--field ${
-          minPrice > maxPrice && "incorrect"
-         }`}
-        >
+        <div className={`filters__filter--field ${minPrice > maxPrice && "incorrect"}`}>
          <span>$</span>
          <input
           min={0}
@@ -179,11 +194,7 @@ const Search: React.FC = () => {
          />
         </div>
         <span>to</span>
-        <div
-         className={`filters__filter--field ${
-          minPrice > maxPrice && "incorrect"
-         }`}
-        >
+        <div className={`filters__filter--field ${minPrice > maxPrice && "incorrect"}`}>
          <span>$</span>
          <input
           min={minPrice || 0}
@@ -197,54 +208,38 @@ const Search: React.FC = () => {
        </div>
       </div>
      </div>
-     <div
-      className={`filters__filter ${
-       filterStates.categories.visible ? "shown" : ""
-      }`}
-     >
+     <div className={`filters__filter ${filterStates.categories.visible ? "shown" : ""}`}>
       <div className="filters__filter--header">
        <span>Categories</span>
        <div
-        className={`filters__filter--show-icon ${
-         filterStates.categories.visible ? "rotated" : ""
-        }`}
+        className={`filters__filter--show-icon ${filterStates.categories.visible ? "rotated" : ""}`}
         onClick={() => handleFilterToggle("categories")}
        >
         <ChevronDownIcon />
        </div>
       </div>
-      <div
-       className={`filters__filter--content ${
-        filterStates.categories.visible ? "shown" : ""
-       }`}
-      >
+      <div className={`filters__filter--content ${filterStates.categories.visible ? "shown" : ""}`}>
        <ul className="filters__category-list">
-        <li className="filters__category-item">
-         <input
-          type="checkbox"
-          id="woman"
-          className="filters__category-checkbox"
-         />
-         <label
-          htmlFor="woman"
-          className="filters__category-label"
+        {allCategories.map((category) => (
+         <li
+          key={category}
+          className="filters__category-item"
          >
-          Woman
-         </label>
-        </li>
-        <li className="filters__category-item">
-         <input
-          type="checkbox"
-          id="man"
-          className="filters__category-checkbox"
-         />
-         <label
-          htmlFor="man"
-          className="filters__category-label"
-         >
-          Man
-         </label>
-        </li>
+          <input
+           type="checkbox"
+           id={category}
+           className="filters__category-checkbox"
+           checked={categories.includes(category)}
+           onChange={() => handleCategoryChange(category)}
+          />
+          <label
+           htmlFor={category}
+           className="filters__category-label"
+          >
+           {category.charAt(0).toUpperCase() + category.slice(1)}
+          </label>
+         </li>
+        ))}
        </ul>
       </div>
      </div>
