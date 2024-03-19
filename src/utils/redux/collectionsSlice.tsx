bb6f29cba08b987c863542113bf53083c426b877
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-03-19 17:43:28 +0100

    handling status of fetching collections

--- src/utils/redux/collectionsSlice.tsx
+++ src/utils/redux/collectionsSlice.tsx
@@ -4,24 +4,30 @@ import { Collection } from "../../App";
 interface CollectionsState {
  collectionsSlice: {
   collections: Collection[];
+  status: string;
  };
 }
 
 const collectionsSlice = createSlice({
  name: "collections",
  initialState: {
-  collections: []
+  collections: [],
+  status: "loading"
  },
  reducers: {
   fetchCollections: () => {},
   setCollections: (state, { payload }) => {
    state.collections = payload;
+  },
+  setCollectionsStatus: (state, { payload }) => {
+   state.status = payload;
   }
  }
 });
 
-export const { setCollections, fetchCollections } = collectionsSlice.actions;
+export const { setCollections, fetchCollections, setCollectionsStatus } = collectionsSlice.actions;
 
 export const selectCollections = (state: CollectionsState) => state.collectionsSlice.collections;
+export const selectCollectionsStatus = (state: CollectionsState) => state.collectionsSlice.status;
 
 export default collectionsSlice.reducer;
--- src/utils/saga/collectionsSaga.tsx
+++ src/utils/saga/collectionsSaga.tsx
@@ -1,15 +1,18 @@
 import { call, delay, put, takeLatest } from "redux-saga/effects";
 import { getCollections } from "./getCollections";
-import { fetchCollections, setCollections } from "../redux/collectionsSlice";
+import { fetchCollections, setCollections, setCollectionsStatus } from "../redux/collectionsSlice";
 import { Product } from "../../App";
 
 function* collectionsSagaHandler() {
+ yield put(setCollectionsStatus("loading"));
  yield delay(1000);
  try {
   const collections: Product = yield call(getCollections);
   yield put(setCollections(collections));
+  yield put(setCollectionsStatus("success"));
  } catch (e) {
   console.log(e);
+  yield put(setCollectionsStatus("error"));
  }
 }
 
