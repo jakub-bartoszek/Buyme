39bc0d46c7dac6f7c4bb4ba59a2756853e4adeb6
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-03-19 15:31:45 +0100

    mapping collections to render sections

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,31 +1,22 @@
 import React from "react";
 import "./Home.scss";
 import { useSelector } from "react-redux";
-import { selectProducts } from "../../utils/redux/productsSlice";
-import { Product } from "../../App";
+import { Collection } from "../../App";
 import HomeSection from "../../components/HomeSection/HomeSection";
+import { selectCollections } from "../../utils/redux/collectionsSlice";
 
 const Home: React.FC = () => {
- const products: Product[] = useSelector(selectProducts);
+ const collections: Collection[] = useSelector(selectCollections);
 
  return (
   <main className="home">
    <header className="home__banner" />
-   <HomeSection
-    name="newest"
-    products={products.filter((product) => product.date_added.includes("2024"))}
-    image="./images/newest-collection-banner.jpg"
-   />
-   <HomeSection
-    name="most popular"
-    products={products.filter((product) => product.popularity > 80)}
-    image="./images/most-popular-collection-banner.jpg"
-   />
-   <HomeSection
-    name="winter collection"
-    products={products.filter((product) => product.categories.includes("winter"))}
-    image="./images/winter-collection-banner.jpg"
-   />
+   {collections.map((collection) => (
+    <HomeSection
+     name={collection.name}
+     image={collection.image}
+    />
+   ))}
   </main>
  );
 };
