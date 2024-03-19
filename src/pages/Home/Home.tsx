8f3471d603f8f3e6301b5f15b9910931e2bc9c75
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-03-19 17:43:12 +0100

    conditional rendering in HomePage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,22 +3,37 @@ import "./Home.scss";
 import { useSelector } from "react-redux";
 import { Collection } from "../../App";
 import HomeSection from "../../components/HomeSection/HomeSection";
-import { selectCollections } from "../../utils/redux/collectionsSlice";
+import { selectCollections, selectCollectionsStatus } from "../../utils/redux/collectionsSlice";
+import Loader from "../../components/Loader/Loader";
 
 const Home: React.FC = () => {
  const collections: Collection[] = useSelector(selectCollections);
+ const collectionsStatus = useSelector(selectCollectionsStatus);
 
- return (
-  <main className="home">
-   <header className="home__banner" />
-   {collections.map((collection) => (
-    <HomeSection
-     name={collection.name}
-     image={collection.image}
-    />
-   ))}
-  </main>
- );
+ switch (collectionsStatus) {
+  case "loading":
+   return (
+    <main className="home">
+     <main className="home__loader-wrapper">
+      <Loader />
+     </main>
+    </main>
+   );
+  case "success":
+   return (
+    <main className="home">
+     <header className="home__banner" />
+     {collections.map((collection) => (
+      <HomeSection
+       name={collection.name}
+       image={collection.image}
+      />
+     ))}
+    </main>
+   );
+  default:
+   return null;
+ }
 };
 
 export default Home;
