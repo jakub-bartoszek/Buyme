c3da9f879d7f01f38d57f32134d4bdb9833e7e14
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-03-21 14:21:03 +0100

    added products fetch status

--- src/utils/redux/productsSlice.tsx
+++ src/utils/redux/productsSlice.tsx
@@ -4,25 +4,30 @@ import { Product } from "../../App";
 interface ProductsState {
  productsSlice: {
   products: Product[];
+  status: string;
  };
 }
 
 const productsSlice = createSlice({
  name: "products",
  initialState: {
-  products: []
+  products: [],
+  status: "loading"
  },
  reducers: {
   fetchProducts: () => {},
   setProducts: (state, { payload }) => {
    state.products = payload;
+  },
+  setProductsStatus: (state, { payload }) => {
+   state.status = payload;
   }
  }
 });
 
-export const { setProducts, fetchProducts } = productsSlice.actions;
+export const { fetchProducts, setProducts, setProductsStatus } = productsSlice.actions;
 
-export const selectProducts = (state: ProductsState) =>
- state.productsSlice.products;
+export const selectProducts = (state: ProductsState) => state.productsSlice.products;
+export const selectProductsStatus = (state: ProductsState) => state.productsSlice.status;
 
 export default productsSlice.reducer;
