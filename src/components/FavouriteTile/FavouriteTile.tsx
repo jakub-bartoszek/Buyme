561688cf7c52f8dc0c8fb18e5a4941e1ab99c8bd
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-03-24 13:57:19 +0100

    changed order of imports

--- src/components/FavouriteTile/FavouriteTile.tsx
+++ src/components/FavouriteTile/FavouriteTile.tsx
@@ -1,9 +1,9 @@
+import { useDispatch } from "react-redux";
+import { addToFavourites } from "../../utils/redux/favouritesSlice";
 import { Link } from "react-router-dom";
 import { Product } from "../../App";
-import "./FavouriteTile.scss";
 import { TrashIcon } from "@heroicons/react/24/outline";
-import { addToFavourites } from "../../utils/redux/favouritesSlice";
-import { useDispatch } from "react-redux";
+import "./FavouriteTile.scss";
 
 interface FavouriteTileProps {
  product: Product;
